62cafcbad979f571e8d37d6677f6c16e83925a5a
UnaAceituna <oli.oily@yandex.ru>
2022-06-15 21:32:14 +0300

    add

---
+++ 2_Olive_JS.js
@@ -0,0 +1,17 @@
+function checkAge(age){
+    if (age!==String){
+        console.log('You have mistake')
+    }
+        if (age < 18){
+            console.log('You don’t have access cause your age is ' + age + ', it’s less then ' + 18 )
+        }
+        else if (age >= 18 && age <= 60){
+                console.log('Welcome  !')
+            }
+        else if (age > 60){
+                console.log('Keep calm and look Culture channel')
+            }
+        }
+
+
+checkAge(66)
