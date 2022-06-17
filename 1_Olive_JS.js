6b180faa4e33e541c5110179af7676f3b354db98
UnaAceituna <oli.oily@yandex.ru>
2022-06-18 00:31:18 +0300

    *

--- 1_Olive_JS.js
+++ 1_Olive_JS.js
@@ -1,15 +1,23 @@
-function checkAge(age)
+const age1 = 13
+const age2 = 18
+const age3 = 60
+
+const checkAge = function checkAge(age)
 {
-    if (age < 18){
-        console.log('You don’t have access cause your age is ' + age + ', it’s less then ' + 18 )
+    if (age < age2){
+        console.log('You don’t have access cause your age is ' + age + ', it’s less then ' + age2 )
     }
-        else if (age >= 18 && age <= 60){
+        else if (age >= age2 && age < age3){
             console.log('Welcome  !')
         }
-        else if (age > 60){
+        else if (age >= age3){
             console.log('Keep calm and look Culture channel')
+        } 
+        else {
+            console.log('Technical work')
         }
-    
     }
-
-    checkAge(33)
+    checkAge(17)
+    checkAge(18)
+    checkAge(60)
+   
